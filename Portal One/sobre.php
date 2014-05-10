ce9c06f804d3f2433772154c9db26d31cedd9e2c
Diego Rocha <diego.webdg@gmail.com>
2014-05-10 17:15:58 -0300

    Sobre
    Ainda faltando o slider

---
+++ Portal One/sobre.php
@@ -0,0 +1,152 @@
+<?php include "header.php"; ?>  
+	
+	<link rel="stylesheet" href="css/sobre.css">
+	
+    <div class="path">
+        <div class="content-center">
+            <a href="/" title="home">Home</a>
+            <span class="separator">\</span>
+            <a href="/" title="Contato">Contato</a>
+            <span class="separator">\</span>
+            <span class="the-path">Fale conosco</span>
+        </div>
+    </div>
+
+</div>
+<!--End of Slider-->
+
+<!--Content-->
+<div id="content">
+    <div class="content-center">
+        <h1>Sobre a One</h1>
+        
+        <!--Left-->
+        <div class="left">
+            <ul>
+                <li class="active"><a class="item1" href="#" title="Quem somos">Quem somos</a></li>
+                <li><a class="item2" href="#" title="Portfolio">Portfolio</a></li>
+                <li><a class="item3" href="#" title="Imprensa">Imprensa</a></li>
+                <li><a class="item4" href="#" title="Tabalhe conosco">Tabalhe conosco</a></li>
+            </ul>
+        </div>
+        <!--End of Left-->
+        
+        <!--Right-->
+        <div class="right">
+            <!--Quem somos-->
+            <div class="item1">
+                <h2>A evolução de uma geração</h2>
+                <p>Pioneira no mercado, em 60 anos de conquistas, sucessos e credibilidade, é um referencial no setor imobiliário. Com compromisso, qualidade e principalmente o reconhecimento de consumidores, fornecedores, colaboradores e investidores. </p>
+                <p>Pensando em renovar e revitalizar esta posição, a mudança foi uma evolução natural, criando uma identidade visual mais contemporânea, sofisticada e dinâmica. <span class="red">Um posicionamento que visa à melhoria contínua nas relações e serviços para satisfação total de nossos clientes.</span></p>
+                
+                <h2>Um novo tempo, um novo nome</h2>
+                <p>Hoje entendemos que mais que prover uma moradia ou sala comercial, nossa função é estar presente. Consolidando o relacionamento e estabelecendo a confiança mútua, entregando empreendimentos inovadores nas melhores localizações, com garantia e profissionalismo.</p>
+                <p>Crescendo em todos os aspectos e em conexão constante com as mais atuais tendências, surge a nova marca que resume em si todas estas características, a experiência e solidez adquiridas em mais de 65 mil unidades entregues.</p>
+                <p class="red">Assim nasceu a One Desenvolvimento Imobiliário com a experiência conquistada em 60 anos de boa atuação, o DNA e a expertise do nome M. Goldfarb, comprometida a entregar produtos de máxima qualidade, com os melhores projetos e a rigorosa pontualidade.</p>
+                
+                <h2>Uma visão de futuro pautada pelo compromisso</h2>
+                <p>Fortalecer um compromisso social que oferece mais bem-estar, qualidade de vida e plena satisfação a cada um de nossos clientes. Criando uma relação de confiança, segurança e respeito que permeia toda a nossa trajetória e que se renova em cada unidade entregue.
+Muito mais do que mudar de lugar, a ONE DI ajuda você a mudar para melhor.
+ONE Desenvolvimento Imobiliário – Você muda.</p>
+                
+                <h2>Nova missão</h2>
+                <p>A busca incessante pela máxima qualidade, atendimento diferenciado, constantes investimentos em seus profissionais e em pesquisas de novos projetos e fornecedores. 
+Atento aos últimos lançamentos mundiais, oferecendo sempre empreendimentos com o melhor custo-benefício e pontualidade na entrega. Atingir o grau máximo de satisfação de nossos clientes, ser competitiva, estável financeiramente e proporcionar retorno a nossos colaboradores, parceiros e investidores.</p>
+                
+                <h2>Visão de futuro</h2>
+                <p>Em constante crescimento a ONE Desenvolvimento Imobiliário continuará sendo referência no mercado, com altos investimentos em capacitação, pesquisa de novos <span class="red">produtos</span> e ações sociais e de marketing.
+Buscando sempre oferecer os melhores <span class="red">produtos</span>, novas metodologias de trabalho e aprimoramento constante no atendimento e serviços. Com o objetivo único de proporcionar integridade, qualidade de vida e o melhor em satisfação para os clientes, fornecedores e colaboradores.
+Não é simplesmente o que você faz de melhor, mas a evolução em tudo que você representa.</p>
+                
+                <h2>Valores</h2>
+                <p>Liderança e constância de propósitos - Integridade - Ética - Compromisso - Inovação - Foco no cliente e no mercado - Aprendizado Organizacional - Gestão Baseada em Fatos - Valorização do Ser Humano - Orientação para Resultados - Responsabilidade Social - Crescimento Sustentável.</p>
+                
+                <h2>Diferenciais</h2>
+                <h3>Pontualidade</h3>
+                <p>Garantimos a entrega nos prazos estabelecidos.</p>
+                
+                <h3>Produtos diferenciados</h3>
+                <p class="red">Projetos exclusivos, nas melhores localizações e relação custo-benefício.</p>
+                
+                <h3>Consultoria Especializada</h3>
+                <p>Consultores treinados e capacitados para encontrar as melhores soluções e produtos para cada cliente.</p>
+                
+                <h3>Responsabilidade ecológica</h3>
+                <p>Projetos que refletem nossa preocupação com a natureza e sua preservação.</p>
+                
+                <h3>Certificado de qualidade</h3>
+                <p>Fornecedores auditados e certificados garantindo matérias duráveis e de qualidade.</p>
+            </div>
+            <!--End of Quem somos-->
+            
+            <!--Portfolio-->
+            <div class="item2">
+                <h2>Portfolio</h2>
+                <h3 class="red">Os melhores imóveis residenciais e comerciais especialmente para você</h3>
+                <p>A One Desenvolvimento Imobiliário com a experiência conquistada em 60 anos de boa atuação, o DNA e a expertise do nome M. Goldfarb, comprometida a entregar produtos de máxima qualidade, com os melhores projetos e a rigorosa pontualidade. É um referencial no setor imobiliário.</p>
+                <p>Entendemos que mais que prover uma moradia ou sala comercial, nossa função é estar presente. Consolidando o relacionamento e estabelecendo a confiança mútua, entregando empreendimentos inovadores nas melhores localizações, com garantia e profissionalismo.</p>
+                <p>Abaixo você confere nossos lançamentos, imóveis em construção e prontos para morar:</p>
+            </div>
+            <!--End of Portfolio-->
+            
+            <!--Imprensa-->
+            <div class="item3">
+                <h2>Imprensa</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+                <h3>Contatos</h3>
+                
+                <div class="contact">
+                    <span>João Elseve</span>
+                    <span>joaoelseve@one-di.com.br</span>
+                    <span>Tel. 11 1234•9876</span>
+                </div>
+                
+                <div class="contact">
+                    <span>Maria Solange</span>
+                    <span>mariasolange@one-di.com.br</span>
+                    <span>Tel. 11 1234•9876</span>
+                </div>
+            </div>
+            <!--End of Imprensa-->
+            
+            <!--Trabalhe conosco-->
+            <div class="item4">
+                <h2>Fale conosco</h2>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                
+                <h2 style="margin-top:60px;">Envie seu curriculo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                
+                <!--Form-->
+                <div class="form">
+                    <h4>Preencha os campos abaixo</h4>
+                    <form>
+                        <input type="text" name="nome-trabalhe-conosco" id="nome-trabalhe-conosco" value="NOME" onfocus="if (this.value==this.defaultValue) this.value = ''" onblur="if (this.value=='') this.value = this.defaultValue" class="left-input">
+                        <input type="email" name="email-trabalhe-conosco" id="email-trabalhe-conosco" value="E-MAIL" onfocus="if (this.value==this.defaultValue) this.value = ''" onblur="if (this.value=='') this.value = this.defaultValue" class="right-input">
+                        <input type="tel" name="tel-trabalhe-conosco" id="tel-trabalhe-conosco" value="TEL.: (00) 0000•0000" onfocus="if (this.value==this.defaultValue) this.value = ''" onblur="if (this.value=='') this.value = this.defaultValue" class="left-input">
+                        <button type="button" title="Anexar arquivo" id="anexar-arquivo-trabalhe-conosco" class="upload right-input animate">Anexar arquivo</button>
+                        <textarea name="mensagem-trabalhe-conosco" id="mensagem-trabalhe-conosco" onfocus="if (this.value==this.defaultValue) this.value = ''" onblur="if (this.value=='') this.value = this.defaultValue">MENSAGEM</textarea>
+                    <small>*Todos os campos são obrigatórios</small>
+                        <button type="submit" title="Enviar" id="enviar-trabalhe-conosco" class="enviar animate">Enviar</button>
+                    </form>
+                </div>
+                <!--End of Form-->
+            </div>
+            <!--End of Trabalhe conosco-->
+        </div>
+        <!--End of Right-->
+        <div class="clear"></div>
+    </div>
+</div>
+<!--End of Content-->
+
+<!--Scripts-->
+<script type="text/javascript" src="/js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="/js/sobre.js"></script>
+<!--End of Scripts-->
+
+<link rel="stylesheet" href="/js/fancybox/jquery.fancybox.css">
+        
+<?php include "footer.php" ?>
