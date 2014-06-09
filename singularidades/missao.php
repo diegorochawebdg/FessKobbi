1c3398217575c5a2aa104d94a3c4bf09719d520c
diegowebdg <diego.webdg@gmail.com>
2014-06-09 17:58:37 -0300

    missão

---
+++ singularidades/missao.php
@@ -0,0 +1,84 @@
+<?php include "header.php" ?>
+<link rel="stylesheet" href="css/internals.css">
+
+<div class="content-center">
+    
+    <!--Path-->
+    <div class="path">
+        <a href="/" title="home">home</a>
+        <span>></span>
+        <a href="#" title="o instituto">o instituto</a>
+        <span>></span>
+        <a href="#" title="quem somos">quem somos</a>
+        <span>></span>
+        <span>missão, visão e valores</span>
+    </div>
+    <!--Path-->
+    <!--Title-->
+    <div class="title">
+        <h1>O instituto / <strong>Missão, visão e valores</strong></h1>
+    </div>
+    <!--End of Title-->
+    
+    <!--Left-->
+    <div class="left">
+        <!--Left Menu-->
+        <ul class="main-menu">
+            <li>
+                <a href="#" title="Quem somos" class="with-sub-menu animate">Quem somos</a>
+                <ul class="secondary animate2">
+                    <li><a href="#" title="Missão visão">Missão visão</a></li>
+                    <li><a href="#" title="Equipe de gestão">Equipe de gestão</a></li>
+                    <li><a href="#" title="Mantenedora">Mantenedora</a></li>
+                    <li><a href="#" title="Infraestrutura">Infraestrutura</a></li>
+                    <li><a href="#" title="Responsabilidade social">Responsabilidade social</a></li>
+                    <li><a href="#" title="Convênios">Convênios</a></li>
+                </ul>
+            </li>
+            <li><a href="#" title="CPA">CPA</a></li>
+            <li><a href="#" title="Documentos institucionais">Documentos institucionais</a></li>
+            <li><a href="#" title="Avaliações institucionais">Avaliações institucionais</a></li>
+        </ul>
+        <!--End of Left Menu-->
+        
+        <!--Banners-->
+        <div class="banners">
+            <a href="#" title="Graduação singularidades - Vestibular 2014"><img src="images/quem-somos/banner-vestiular-2014.jpg" width="210" height="191" alt="Graduação singularidades - Vestibular 2014" title="Graduação singularidades - Vestibular 2014"></a>
+            <a href="#" title="Programa de bolsas Fies e Prouni - Transforme o seu futuro"><img src="images/quem-somos/banner-fies-prouni.jpg" width="210" height="191" alt="Programa de bolsas Fies e Prouni - Transforme o seu futuro" title="Programa de bolsas Fies e Prouni - Transforme o seu futuro"></a>
+        </div>
+        <!--End of Banners-->
+    </div>
+    <!--End of Left-->
+    
+    <!--Right-->
+    <div class="right">
+        <!--Banner-->
+        <div class="banner">
+            <img src="images/quem-somos/banner.jpg" width="694" height="211" alt="Instituto Singularidades - Formando professores, transformando o futuro" title="Instituto Singularidades - Formando professores, transformando o futuro">
+        </div>
+        <!--End of Banner-->
+        
+        <!--Text-->
+        <div class="text">
+            <h2>Missão, visão e valores</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie, metus vitae eleifend interdum, ipsum nisl luctus erat, eget laoreet purus felis ut risus. Vivamus elit risus, malesuada sit amet tellus ac, convallis suscipit nibh. Nulla et convallis arcu. Aenean magna nunc, euismod eu tellus eu, vehicula egestas neque. Ut ante ipsum, laoreet vitae pulvinar ut, blandit ut nisi.</p>
+            
+            <h3>Missão</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie, metus vitae eleifend interdum, ipsum nisl luctus erat, eget laoreet purus felis ut risus. Vivamus elit risus, malesuada sit amet tellus ac, convallis suscipit nibh. Nulla et convallis arcu. Aenean magna nunc, euismod eu tellus eu, vehicula egestas neque. Ut ante ipsum, laoreet vitae pulvinar ut, blandit ut nisi.</p>
+            
+            <h3>Visão</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie, metus vitae eleifend interdum, ipsum nisl luctus erat, eget laoreet purus felis ut risus. Vivamus elit risus, malesuada sit amet tellus ac, convallis suscipit nibh. Nulla et convallis arcu. Aenean magna nunc, euismod eu tellus eu, vehicula egestas neque. Ut ante ipsum, laoreet vitae pulvinar ut, blandit ut nisi. </p>
+            
+            <h3>Valores</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas molestie, metus vitae eleifend interdum, ipsum nisl luctus erat, eget laoreet purus felis ut risus. Vivamus elit risus, malesuada sit amet tellus ac, convallis suscipit nibh. Nulla et convallis arcu. Aenean magna nunc, euismod eu tellus eu, vehicula egestas neque. Ut ante ipsum, laoreet vitae pulvinar ut, blandit ut nisi.</p>
+        </div>
+        <!--End of Text-->
+        
+    </div>
+    <!--End of Right-->
+    <div class="clear"></div>
+    
+</div>
+
+<?php include "footer.php" ?>
+<script type="text/javascript" src="js/internal.js"></script>
