ebfef66026a9812bcd238a7d1a3024711ebf12cd
diegowebdg <diego.webdg@gmail.com>
2014-09-01 19:01:04 -0300

    Divena
    Busca de veículos;
    Busca de veículos - interna;

---
+++ Divena/Site/busca-carro-interna.php
@@ -0,0 +1,165 @@
+<?php include "header.php"; ?>  
+	
+	<link rel="stylesheet" href="css/busca-carro.css">
+
+    <!--Slogan-->
+    <div class="slogan">
+        <div class="content-center">
+            <h2>Seminovos Divena</h2>
+            <p>Os melhores seminovos Divena você encontra aqui.</p>
+        </div>
+    </div>
+    <!--End Slogan-->
+
+    <div id="content">
+        <div class="content-center">
+            <!-- Path -->
+            <div class="path">
+                <ul>
+                    <li><a href="/" title="Home">Home</a></li>
+                    <li><span>Seminovos</span></li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+            <!-- End Path -->
+
+            <!-- Side Menu -->
+            <div id="side-menu">
+                <h2>Filtre sua busca</h2>
+                <div class="side-menu-content">
+                    <ul>
+                        <li>
+                            <h3>Marca</h3>
+                            <ul>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe A <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe B <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe C <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe D <span>(12)</span></label>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Modelo</h3>
+                            <ul>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe A <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe B <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe C <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe D <span>(12)</span></label>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Faixa de preço</h3>
+                            <ul>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$50.001 a 100.00 <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$50.001 a 100.00 <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$50.001 a 100.00 <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$500.001 a 800.00 <span>(12)</span></label>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End Side Menu -->
+
+            <!-- The Car -->
+            <div id="the-car">
+                <!-- Slider -->
+                <div id="slider">
+                    <div id="thumbs">
+                        <ul>
+                            <li><a href="#"><img src="http://placehold.it/79x59"></a></li>
+                            <li><a href="#"><img src="http://placehold.it/79x59"></a></li>
+                            <li><a href="#"><img src="http://placehold.it/79x59"></a></li>
+                            <li><a href="#"><img src="http://placehold.it/79x59"></a></li>
+                            <li><a href="#"><img src="http://placehold.it/79x59"></a></li>
+                        </ul>
+                    </div>
+                    <div id="large-img">
+                        <img src="http://placehold.it/606x373">
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <!-- End Slider -->
+
+                <!-- Description -->
+                <div id="description">
+                    <h1>E 350 COUPE - 2011 Gasolina</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nobis assumenda, hic optio minus animi veniam distinctio dignissimos repudiandae! Repellendus aut eius nam deserunt officiis nisi architecto quasi cumque voluptatem.</p>
+                </div>
+                <!-- End Description -->
+
+                <!-- Price -->
+                <div id="price">
+                    <strong>R$ 185.000,00</strong>
+                    <a href="#" title="Solicite mais informações">Solicite mais informações</a>
+                </div>
+                <!-- End Price -->
+                <div class="clear"></div>
+
+                <!-- Opcionais | Descrição -->
+                <div id="opcionais-descricao">
+
+                    <div class="internal-menu">
+                        <ul>
+                            <li class="active item1"><a href="#" title="Opcionais">Opcionais</a></li>
+                            <li class="item2"><a href="#" title="Descrição">Descrição</a></li>
+                        </ul>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="txt">
+                        <div class="item1">
+                            <ul>
+                                <li><strong>Kilometragem:</strong> 18.500km</li>
+                                <li><strong>Portas:</strong> 2</li>
+                                <li><strong>Cor:</strong> Preto</li>
+                                <li><strong>Combustível:</strong> Gasolina</li>
+                                <li><strong>Placa:</strong> EQY1414</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- End Opcionais | Descrição -->
+            </div>
+            <!-- End The Car -->
+            <div class="clear"></div>
+        </div>
+    </div>
+        
+<?php include "footer.php" ?>
---
+++ Divena/Site/busca-carro.php
@@ -0,0 +1,158 @@
+<?php include "header.php"; ?>  
+	
+	<link rel="stylesheet" href="css/busca-carro.css">
+
+    <!--Slogan-->
+    <div class="slogan">
+        <div class="content-center">
+            <h2>Seminovos Divena</h2>
+            <p>Os melhores seminovos Divena você encontra aqui.</p>
+        </div>
+    </div>
+    <!--End Slogan-->
+
+    <div id="content">
+        <div class="content-center">
+            <!-- Path -->
+            <div class="path">
+                <ul>
+                    <li><a href="/" title="Home">Home</a></li>
+                    <li><span>Seminovos</span></li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+            <!-- End Path -->
+
+            <!-- Side Menu -->
+            <div id="side-menu">
+                <h2>Filtre sua busca</h2>
+                <div class="side-menu-content">
+                    <ul>
+                        <li>
+                            <h3>Marca</h3>
+                            <ul>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe A <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe B <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe C <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe D <span>(12)</span></label>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Modelo</h3>
+                            <ul>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe A <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe B <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe C <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">Classe D <span>(12)</span></label>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Faixa de preço</h3>
+                            <ul>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$50.001 a 100.00 <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$50.001 a 100.00 <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$50.001 a 100.00 <span>(12)</span></label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" name="" id="nome-teste">
+                                    <label for="nome-teste">R$500.001 a 800.00 <span>(12)</span></label>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End Side Menu -->
+
+            <!-- Car list -->
+            <div id="car-list">
+                <ul>
+                    <li>
+                        <div class="description animate">
+                            <div class="box-padding">
+                                <h3>E 350 Coupe - 2011</h3>
+                                <span>Preto - 18500km</span>
+                                <span>2 Portas</span>
+                                <span>Gasolina</span>
+                                <strong>R$ 185.000,00</strong>
+                                <a href="#" title="Conheça esse modelo">Conheça esse modelo</a>
+                            </div>
+                        </div>
+                        <a class="image">
+                            <img src="http://placehold.it/442x272">
+                            <div class="conheca animate">
+                                <span>Conheça este modelo</span>
+                            </div>
+                        </a>
+                        <div class="clear"></div>
+                    </li>
+                    <li>
+                        <div class="description animate">
+                            <div class="box-padding">
+                                <h3>E 350 Coupe - 2011</h3>
+                                <span>Preto - 18500km</span>
+                                <span>2 Portas</span>
+                                <span>Gasolina</span>
+                                <strong>R$ 185.000,00</strong>
+                                <a href="#" title="Conheça esse modelo">Conheça esse modelo</a>
+                            </div>
+                        </div>
+                        <a href="#" class="image">
+                            <img src="http://placehold.it/442x272">
+                            <div class="conheca animate">
+                                <span>Conheça este modelo</span>
+                            </div>
+                        </a>
+                        <div class="clear"></div>
+                    </li>
+                </ul>
+
+                <div id="pagination">
+                    <ul>
+                        <li><a href="#" title="Primeiro">Primeiro</a></li>
+                        <li class="active"><a href="#" title="1">1</a></li>
+                        <li><a href="#" title="2">2</a></li>
+                        <li><a href="#" title="3">3</a></li>
+                        <li><a href="#" title="Último">Último</a></li>
+                    </ul>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            <!-- End Car list -->
+            <div class="clear"></div>
+        </div>
+    </div>
+        
+<?php include "footer.php" ?>
